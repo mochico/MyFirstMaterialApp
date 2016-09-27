055fe54633b4333b29796018622e30f9bc525da9
mochico <mochico@pepabo.com>
2016-09-27 15:34:35 +0900

    fix detail Activity

---
+++ app/src/main/java/app/example/com/first/material/design/Item.java
@@ -0,0 +1,33 @@
+package app.example.com.first.material.design;
+
+import java.io.Serializable;
+
+public class Item extends Object implements Serializable {
+    private String title;
+    private String imageName;
+    private String description;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
